d6a3f973dd4da846433f78ace252339412fb4c01
LoganLS <logan_56@hotmail.fr>
2020-06-05 16:49:46 +0200

    lister notes avec api

---
+++ src/components/NotesComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        {{notes}}
+    </div>
+</template>
+
+<script>
+const urlApi = 'http://localhost:3000/notes';
+
+export default {
+  data(){
+    return {
+      notes: ''
+    }
+  },
+  name: 'Notes',
+  created () {
+    this.getNotes()
+  },
+  methods: {
+    getNotes () {
+      fetch(urlApi, {
+        method: 'GET'
+      })
+        .then(response => {
+          return response.json()
+        })
+        .then(response => {
+          this.notes = response
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Notes.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="notes">
+        ici les notes
+        <NotesComponent/>
+    </div>
+</template>
+
+<script>
+import NotesComponent from '@/components/NotesComponent.vue'
+
+export default {
+  name: 'Notes',
+  components: {
+    NotesComponent
+  }
+}
+</script>
